ab402cfbe25fbf5144611220792b5c1542af8a83
Mark McTamney <30511006+markmctamney@users.noreply.github.com>
2020-06-07 21:46:10 -0400

    Add dark theme support

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DarkTheme,
+  DefaultTheme,
+} from '@react-navigation/native';
+import { useColorScheme } from 'react-native';
 
 import BottomTabs from './BottomTabs';
 
 export default function NavigationRoot() {
+  const scheme = useColorScheme();
+
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
       <BottomTabs />
     </NavigationContainer>
   );
